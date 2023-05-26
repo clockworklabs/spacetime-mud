fee89b7ec9033b8dc9f7f7d57338360d45e6e951
Derek Brinkmann <dbrinkmann@citadelstudios.net>
2023-05-26 14:32:10 -0400

    Initial commit

---
+++ react-client/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="text-area"></div>
+      <input type="text" className="input-area" />
+    </div>
+  );
+}
+
+export default App;
